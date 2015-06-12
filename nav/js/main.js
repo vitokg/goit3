e14be188eb873341c384abe7d55e07f4488298a6
igordubovoy <idubovoy90@gmail.com>
2015-06-12 21:34:49 +0300

    +js

---
+++ nav/js/main.js
@@ -0,0 +1,18 @@
+(function(){
+    var headerH = 200,
+        menu = document.querySelector('#navigation');
+        menu.style.cssText += " position: fixed; top: "+ headerH+"px";
+
+        window.addEventListener('scroll', scrollNav, false);
+
+        function scrollNav() {
+            var top = window.scrollY
+            console.log(top);
+            if(top < headerH) {
+            menu.style.cssText += "top: "+ (headerH-top)+"px";
+            }
+            else{  menu.style.cssText += "top:0";
+            }
+        }
+})()
+
