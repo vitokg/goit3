3c279f8e2f89887a40035fd59f2ec35257bf6e60
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-07-25 12:56:58 +0300

    task8

--- js_vacation/task8/js.js
+++ js_vacation/task8/js.js
@@ -1,3 +1,15 @@
 /**
  * Created by ProFi on 25.07.2015.
  */
+
+function helloWorld() {
+    if (document.getElementsByName('lang')[0].checked) {
+        document.getElementById('hello').value='Hello, World!';
+    } else if (document.getElementsByName('lang')[1].checked) {
+        document.getElementById('hello').value='Hola, Mundo!';
+    } else {
+        document.getElementById('hello').value='Hallo, Welt!';
+
+    }
+
+}
