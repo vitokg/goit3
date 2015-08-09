3b5f3bb58cfdab84ad53e25a31a8f1c7809b759a
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-09 18:42:20 +0300

    update

--- js_vacation/task16/js.js
+++ js_vacation/task16/js.js
@@ -3,60 +3,64 @@
  */
 
 function whatToDo() {
-    var time=parseInt(document.getElementById('time').value);
+    var time = parseInt(document.getElementById('time').value);
+    var sorry = 'Sorry, you値l have to wait or go get a snack.';
+    var breakfast = 'Breakfast is served.';
+    var lunch = 'Time for lunch.';
+    var dinner = 'It痴 dinner time.';
     if (document.getElementsByName('timePeriod')[0].checked) {
         switch (time) {
-            case 0: document.getElementById('answer').value='Sorry, you値l have to wait or go get a snack.';
+            case 0: document.getElementById('answer').value = sorry;
                 break
-            case 1: document.getElementById('answer').value='Sorry, you値l have to wait or go get a snack.';
+            case 1: document.getElementById('answer').value = sorry;
                 break
-            case 2: document.getElementById('answer').value='Sorry, you値l have to wait or go get a snack.';
+            case 2: document.getElementById('answer').value = sorry;
                 break
-            case 3: document.getElementById('answer').value='Sorry, you値l have to wait or go get a snack.';
+            case 3: document.getElementById('answer').value = sorry;
                 break
-            case 4: document.getElementById('answer').value='Sorry, you値l have to wait or go get a snack.';
+            case 4: document.getElementById('answer').value = sorry;
                 break
-            case 5: document.getElementById('answer').value='Sorry, you値l have to wait or go get a snack.';
+            case 5: document.getElementById('answer').value = sorry;
                 break
-            case 6: document.getElementById('answer').value='Breakfast is served.';
+            case 6: document.getElementById('answer').value = breakfast;
                 break
-            case 7: document.getElementById('answer').value='Breakfast is served.';
+            case 7: document.getElementById('answer').value = breakfast;
                 break
-            case 8: document.getElementById('answer').value='Breakfast is served.';
+            case 8: document.getElementById('answer').value = breakfast;
                 break
-            case 9: document.getElementById('answer').value='Breakfast is served.';
+            case 9: document.getElementById('answer').value = breakfast;
                 break
-            case 10: document.getElementById('answer').value='Sorry, you値l have to wait or go get a snack.';
+            case 10: document.getElementById('answer').value = sorry;
                 break
-            case 11: document.getElementById('answer').value='Time for lunch.';
+            case 11: document.getElementById('answer').value = lunch;
                 break
-            case 12: document.getElementById('answer').value='Time for lunch.';
+            case 12: document.getElementById('answer').value = lunch;
                 break
         }
     }
     if (document.getElementsByName('timePeriod')[1].checked) {
         switch (time) {
-            case 1: document.getElementById('answer').value='Time for lunch.';
+            case 1: document.getElementById('answer').value = lunch;
                 break
-            case 2: document.getElementById('answer').value='Sorry, you値l have to wait or go get a snack.';
+            case 2: document.getElementById('answer').value = sorry;
                 break
-            case 3: document.getElementById('answer').value='Sorry, you値l have to wait or go get a snack.';
+            case 3: document.getElementById('answer').value = sorry;
                 break
-            case 4: document.getElementById('answer').value='Sorry, you値l have to wait or go get a snack.';
+            case 4: document.getElementById('answer').value = sorry;
                 break
-            case 5: document.getElementById('answer').value='It痴 dinner time.';
+            case 5: document.getElementById('answer').value = dinner;
                 break
-            case 6: document.getElementById('answer').value='It痴 dinner time.';
+            case 6: document.getElementById('answer').value = dinner;
                 break
-            case 7: document.getElementById('answer').value='It痴 dinner time.';
+            case 7: document.getElementById('answer').value = dinner;
                 break
-            case 8: document.getElementById('answer').value='It痴 dinner time.';
+            case 8: document.getElementById('answer').value = dinner;
                 break
-            case 9: document.getElementById('answer').value='Sorry, you値l have to wait or go get a snack.';
+            case 9: document.getElementById('answer').value = sorry;
                 break
-            case 10: document.getElementById('answer').value='Sorry, you値l have to wait or go get a snack.';
+            case 10: document.getElementById('answer').value = sorry;
                 break
-            case 11: document.getElementById('answer').value='Sorry, you値l have to wait or go get a snack.';
+            case 11: document.getElementById('answer').value = sorry;
                 break
         }
     }
