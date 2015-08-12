524805ad63a3b796cc46c35a872022cdfe4c2c06
OleksandrSachuk <ovsachuk@!gmail.com>
2015-08-12 15:57:04 +0300

    12-08-2015

--- JavaScript/Home tasks/Home task 3/task 3-5/task 3-5.js
+++ JavaScript/Home tasks/Home task 3/task 3-5/task 3-5.js
@@ -1,23 +1,25 @@
-/*
-Не работает
-
+//variables declaration
 var hesh = "#";
 var whiteSpace = " ";
 var string = "";
-next:
-for(var i = 1; i <= 4; i++) {
-    if (i % 2 == 0) {
-        for (var j = 1; j < 5; j++) {
-            string = string + whiteSpace + hesh;
-        }
 
-    } else {
-        for (var j = 1; j < 5; j++) {
-            string = string + hesh + whiteSpace;
-        }
+//cycle for 8 horizontal strings
+for(var i = 1; i <= 8; i++) {
 
+    //create empty string
+    string = "";
+
+    //cycle for creation a single string
+    for(var j = 1; j<= 4; j++){
+
+        //every even row will be start by white space
+        if(i%2 == 0){
+            string += whiteSpace + hesh;
+
+            //every odd row will be start by hesh
+        }else {
+            string +=hesh + whiteSpace;
+        }
     }
     console.log(string);
-
 }
-*/
