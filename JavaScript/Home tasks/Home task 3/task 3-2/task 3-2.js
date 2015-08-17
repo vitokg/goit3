29bbe6404f23848cf8c46abbed54f140efb95188
OleksandrSachuk <ovsachuk@!gmail.com>
2015-08-17 17:19:09 +0300

    17-08-2015

--- JavaScript/Home tasks/Home task 3/task 3-2/task 3-2.js
+++ JavaScript/Home tasks/Home task 3/task 3-2/task 3-2.js
@@ -1,15 +1,18 @@
+var iStart = 2;
+var iFinish = 100;
+
 //create label
 next:
 
 //start cycle from 2 to 10
-for (var i = 2; i < 10; i++ ) {
+for (var i = iStart; i < iFinish; i++ ) {
 
     //start new cycle from 2 to 10 where j = i - 1
-    for (var j = 2; j < i; j++) {
+    for (var j = iStart; j < i; j++) {
 
         //if i % j == 0 start main cycle with the label next again
         if (i % j == 0) continue next;
     }
     //if i % j != 0 alert(i)
-    alert(i);
+    console.log(i);
 }
--- JavaScript/Home tasks/Home task 3/task 3-4/task 3-4.js
+++ JavaScript/Home tasks/Home task 3/task 3-4/task 3-4.js
@@ -1,4 +1,22 @@
 for (var i = 1; i <= 100; i++) {
+    switch (i) {
+        case 15:
+        case 30:
+        case 45:
+        case 60:
+        case 75:
+        case 90:
+            console.log("FizzBuzz");
+            break;
+        default:
+            console.log(i);
+            break;
+    }
+}
+
+
+/*
+for (var i = 1; i <= 100; i++) {
 
 //show "FizzBuzz" if (i % 3 == 0 && i % 5 == 0 )
     if (i % 3 == 0 && i % 5 == 0 ) {
@@ -9,3 +27,4 @@ for (var i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+ */
