0519f673c600c5671f9c186d277f9af75c8267d0
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-17 18:08:17 +0300

    task23

--- js_vacation/task23/js.js
+++ js_vacation/task23/js.js
@@ -2,3 +2,19 @@
  Write a function that takes a character (i.e. a string of length 1)
  and returns true if it is a vowel, false otherwise.
  */
+
+(function (character) {
+    var vowel = new Array("A", "E", "I", "O", "U", "Y");
+    var result;
+    for (var i = 0; i < vowel.length; i++) {
+        if (character == vowel[i]) {
+            result = true;
+            break;
+        } else {
+            result = false;
+        }
+    }
+    console.log(result);
+})("Y");
+
+
