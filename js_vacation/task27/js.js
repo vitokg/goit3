91921488950d4c2f98ab6d7a343a2233853ae51b
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-18 17:01:01 +0300

    task27

---
+++ js_vacation/task27/js.js
@@ -0,0 +1,6 @@
+/**
+ Represent a small bilingual lexicon as a Javascript object in the following
+ fashion {"merry":"god", "christmas":"jul", "and":"och", "happy":gott",
+ "new":"nytt", "year":"år"} and use it to translate your Christmas cards
+ from English into Swedish.
+ */
