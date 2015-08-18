8a2dcb6814afc4ccd7c2dc42197a1237218ba004
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-18 14:54:05 +0300

    update

--- js_vacation/task24/js.js
+++ js_vacation/task24/js.js
@@ -4,3 +4,42 @@
  occurrence of "o" in between. For example, translate("this is fun")
  should return the string "tothohisos isos fofunon".
  */
+
+function newString(myString) {
+    var vowel=new Array("a", "e", "i", "o", "u", "y");
+    var newString = new Array(50);
+    for (var i=0; i<myString.length; i++) {
+        for (var j = 0; j < newString.length; j++) {
+            if (myString[i] == vowel[i]) {
+                newString[j] = myString[i];
+            } else {
+                newString[j] = myString[i];
+                newString[j + 1] = "o";
+            }
+        }
+    }
+    console.log(newString);
+}
+newString("this is fun");
+
+
+/*for (var i = 0; i < myString.length; i++) {
+ for (var k = 0; k < newString.length; k++) {
+ for (var j = 0; j < vowel.length; j++) {
+
+ if (myString[i] == vowel[j]) {
+ newString[k] = myString[i];
+ } else {
+ newString[k] = myString[i];
+ newString[k + 1] = "o";
+ newString[k + 2] = myString[i];
+ k = k + 3;
+ }
+ console.log(newString[k]);
+ }
+ }*/
+
+/*
+ }
+ }
+    */
