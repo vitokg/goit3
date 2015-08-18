a5ae37cd76f87e278a482b6b688731dfb733e204
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-18 16:47:43 +0300

    task25

--- js_vacation/task24/js.js
+++ js_vacation/task24/js.js
@@ -6,7 +6,7 @@
  */
 
 function newString(myString) {
-    var vowel=new Array("a", "e", "i", "o", "u", "y");
+    var vowel=["a", "e", "i", "o", "u", "y"];
     var newString = new Array(50);
     for (var i=0; i<myString.length; i++) {
         for (var j = 0; j < newString.length; j++) {
@@ -22,24 +22,3 @@ function newString(myString) {
 }
 newString("this is fun");
 
-
-/*for (var i = 0; i < myString.length; i++) {
- for (var k = 0; k < newString.length; k++) {
- for (var j = 0; j < vowel.length; j++) {
-
- if (myString[i] == vowel[j]) {
- newString[k] = myString[i];
- } else {
- newString[k] = myString[i];
- newString[k + 1] = "o";
- newString[k + 2] = myString[i];
- k = k + 3;
- }
- console.log(newString[k]);
- }
- }*/
-
-/*
- }
- }
-    */
--- js_vacation/task25/js.js
+++ js_vacation/task25/js.js
@@ -4,3 +4,30 @@
  For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4])
  should return 24.
  */
+
+var newArray = new Array(10);
+function fillArray() {
+    for (var i = 0; i < newArray.length; i++) {
+        newArray[i] = Math.random() * 10;
+        console.log(newArray[i]);
+    }
+}
+fillArray();
+
+function sumArray() {
+    var sum = 0;
+    for (var i = 0; i < newArray.length; i++) {
+        sum = sum + newArray[i];
+    }
+    console.log("Sum="+sum);
+}
+sumArray();
+
+function multiplyArray() {
+    var multiply=1;
+    for (var i=0; i<newArray.length; i++) {
+        multiply=multiply*newArray[i];
+    }
+    console.log("Multiply="+multiply);
+}
+multiplyArray();
