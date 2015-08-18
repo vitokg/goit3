28f61e1eb507690719cf8ee9242474786386f416
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-08-18 17:16:15 +0300

    task27

--- js_vacation/task27/js.js
+++ js_vacation/task27/js.js
@@ -4,3 +4,13 @@
  "new":"nytt", "year":"år"} and use it to translate your Christmas cards
  from English into Swedish.
  */
+
+var christmasCard = {
+    "Merry": "God",
+    "Christmas": "Jul",
+    "and": "och",
+    "happy": "gott",
+    "New": "Nytt",
+    "Year": "er"
+}
+console.log(christmasCard.Merry+" "+christmasCard.Christmas+" "+christmasCard.and+" "+christmasCard.happy+" "+christmasCard.New+" "+christmasCard.Year);
