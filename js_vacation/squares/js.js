094df0378b57f7b3de78fb71baa9963d2f87fb2c
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-09-09 20:04:43 +0300

    lesson10

---
+++ js_vacation/squares/js.js
@@ -0,0 +1,14 @@
+function square(n) {
+var block=[];
+    for (var i=0; i<block.length; i++) {
+        block[i]=document.getElementsByClassName("square");
+    }
+    for (var i=0; i<block.length; i++) {
+        for (var j=0; j<n; j++) {
+            block[i].document.getElementsByClassName("square").style.float="left";
+
+        }
+    }
+
+}
+square();
