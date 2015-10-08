fcae0ddec6b5f3e4eb9d952001e00a30267fb615
nkartovskaya@gmail.com <Asdfgh0965633354>
2015-10-08 13:22:03 +0300

    update

--- js_vacation/squares/js.js
+++ js_vacation/squares/js.js
@@ -1,14 +1,36 @@
-function square(n) {
-var block=[];
-    for (var i=0; i<block.length; i++) {
-        block[i]=document.getElementsByClassName("square");
-    }
-    for (var i=0; i<block.length; i++) {
-        for (var j=0; j<n; j++) {
-            block[i].document.getElementsByClassName("square").style.float="left";
+window.addEventListener("load", renderGrid, false);
+window.addEventListener("resize", renderGrid, false);
+function renderGrid() {
+    var blocks = document.getElementById("gridContainer").children;
+    var blockAmount=10;
+    var columns=3;
+    var newLeftPosition, newTopPosition;
+    for (var i=1; i<blocks.length; i++) {
 
+
+        if (i%columns==0) {
+            newTopPosition=(getTop(blocks[i-columns])+getLeft(blocks[i-columns]))+blockAmount;
+            blocks[i].style.top=newTopPosition+"px";
+        } else {
+            if (blocks[i-columns]) {
+                newTopPosition=(getTop(blocks[i-columns])+getLeft(blocks[i-columns]))+blockAmount;
+                blocks[i].style.top=newTopPosition+"px";
+            }
+            newLeftPosition=(getTop(blocks[i-1])+getLeft(blocks[i-1]))+blockAmount;
+            blocks[i].style.left=newLeftPosition+"px";
         }
     }
-
 }
-square();
+
+function getTop(element) {
+    var box = element.getBoundingClientRect();
+    return box.top + pageYOffset;
+}
+
+function getLeft(element) {
+    var  box=element.getBoundingClientRect();
+    return box.left + pageXOffset;
+}
+console.log(getTop(document.getElementById("square")));
+
+
